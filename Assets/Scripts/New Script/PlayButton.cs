b8fe0e9c63827e9132cfc19da8223a4d0255b046
Nerson Madriaga <nersonmadriaga.taktyl@gmail.com>
2020-03-14 18:01:07 +0800

    Changes: Added UI Animation, Added Play Button Script

---
+++ Assets/Scripts/New Script/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject panel2;
+
+    public void buttonPlay()
+    {
+        panel2.SetActive(true);
+    }
+}
