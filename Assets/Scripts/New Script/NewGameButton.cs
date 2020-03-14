fd8fa83fac3d406d2d1ca55cbda1df47f0cbb7a9
Nerson Madriaga <nersonmadriaga.taktyl@gmail.com>
2020-03-14 18:10:00 +0800

    Changed: Added New Game Button

--- Assets/Scripts/New Script/NewGameButton.cs
+++ Assets/Scripts/New Script/NewGameButton.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewGameButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void newGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Stage 1");
     }
 }
