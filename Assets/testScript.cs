770eb55901593d38111972aacb3430e9c6c6661a
Nerson Madriaga <nersonmadriaga@gmail.com>
2020-03-16 00:51:40 +0800

    Changed: Added Scripts and Scenes for testing

---
+++ Assets/testScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class testScript : MonoBehaviour
+{
+    private bool moveRight = false;
+    private int speed = 5;
+    void Update()
+    {
+        if(MoveRight)
+        {
+            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+    }
+
+    public bool MoveRight
+    {
+        get{return moveRight;}
+        set{moveRight = value;}
+
+    }
+}
---
+++ Assets/testScript2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript2 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+     testScript t = new testScript();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+}
