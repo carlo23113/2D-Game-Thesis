7d8de03844bcb04f668f9451b3aac3c928c60363
Nerson Madriaga <nersonmadriaga.taktyl@gmail.com>
2020-04-14 00:14:37 +0800

    Changed: Player movements complete

---
+++ Assets/New Script/LeftButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LeftButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool Pressed;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+        
+    }
+}
--- Assets/New Script/PlayerMovement.cs
+++ Assets/New Script/PlayerMovement.cs
@@ -6,53 +6,88 @@ public class PlayerMovement : MonoBehaviour
 {
     
     private Rigidbody2D rigidBody;
-    [SerializeField]private FixedButton button;
+    private Animator animator;
+    [SerializeField]private RightButton btn_right;
+    [SerializeField]private LeftButton btn_left;
+    [SerializeField]private JumpButton btn_jump;
     [SerializeField]private float speed;
-    private float idle = 0f;
-    
-
+    [SerializeField]private float jumpForce;
+    private float idle = 0.0f;
+    private float run = 0.5f;
+    private float jumpUp = 1.0f;
+    private float jumpDown = 1.5f;
+    private float facingRight = 1f;
+    private bool isJumping;
+    private Vector3 direction;
     void Start(){
         rigidBody = GetComponent<Rigidbody2D>();
-        //button = GetComponent<FixedButton>();
+        animator = GetComponent<Animator>();
+        direction = transform.localScale;
     }
 
     void FixedUpdate(){
-
-        if(button.Pressed){
-           
-            rigidBody.velocity = new Vector2(speed * button.Moved, rigidBody.velocity.y);
-            
-        }else{
-            rigidBody.velocity = new Vector2(speed * idle, rigidBody.velocity.y);
+        /*
+        Jump();
+        MovedForward();
+        MovedBackward();
+        */
+        if(btn_right.Pressed){
+            MovedForward();
+        }
+        else if(btn_left.Pressed){
+            MovedBackward();
+        }
+        else {
+            Stay();
         }
 
+        if(btn_jump.Pressed){
+            
+            Jump();
+        }
+
+        if(isJumping){
+            animator.SetFloat("status",jumpUp);
+            animator.SetFloat("status",jumpDown);
+        }
         
-       
     }
 
-    /*
-    private Rigidbody2D rigidBody;
-    private float direction;
-    [SerializeField]private float speed;
-
-    public float Direction
-    {
-        get{return direction;}
-        set{direction = value;}
+    private void Stay(){
+         rigidBody.velocity = new Vector2(speed * idle, rigidBody.velocity.y);
+         animator.SetFloat("status", idle);
+    }
+    private void MovedForward(){
+        
+            rigidBody.velocity = new Vector2(speed * facingRight, rigidBody.velocity.y);
+            direction.x = facingRight;
+            transform.localScale = direction;
+            animator.SetFloat("status", run);
+        
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidBody = GetComponent<Rigidbody2D>();
-
+    private void MovedBackward(){
+        
+            rigidBody.velocity = new Vector2(speed * -facingRight, rigidBody.velocity.y);
+            direction.x = -facingRight;
+            transform.localScale = direction;
+            animator.SetFloat("status", run);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        rigidBody.velocity = new Vector2(direction * speed, rigidBody.velocity.y);
+    private void Jump(){
+         
+        isJumping = true;
+        
+        rigidBody.velocity = Vector2.up * jumpForce;
+        
     }
 
-    */
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Ground")){
+            isJumping = false;
+            animator.SetFloat("status",idle);
+            rigidBody.velocity = Vector2.zero;
+            
+        }
+    }
 }
