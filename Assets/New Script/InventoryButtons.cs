c7555d6d152e327e4f0c7892804cae725bbfefb9
Nerson Madriaga <nersonmadriaga.taktyl@gmail.com>
2020-04-14 14:05:04 +0800

    Changed: Improved Inventory UI, Added new sprites

---
+++ Assets/New Script/InventoryButtons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryButtons : MonoBehaviour
+{
+    [SerializeField]private GameObject inventory;
+    public void OpenInventory(){
+        inventory.SetActive(true);
+    }
+
+    public void CloseInventory(){
+        inventory.SetActive(false);
+    }
+}
